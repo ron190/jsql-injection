b2fb36ff1d1e73e49835fcedfad5ad8a72c5f747
ron190 <ron190@ymail.com>
2019-12-25 18:10:29 +0100

    Fix Groovy imports

--- src/test/java/groovy/JsonUtilSpock.groovy
+++ src/test/java/groovy/JsonUtilSpock.groovy
@@ -1,9 +1,12 @@
 package groovy
 
 import java.util.AbstractMap.SimpleEntry
+import java.util.stream.Collectors
 
 import org.json.JSONArray
 import org.json.JSONObject
+
+import com.jsql.util.JsonUtil
 
 import spock.lang.Specification
 
