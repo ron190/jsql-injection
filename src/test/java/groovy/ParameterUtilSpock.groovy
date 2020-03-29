7097ad96aae1b388d0d1d556b000faa2ce5fc752
ron190 <ron190@ymail.com>
2020-03-30 00:09:46 +0200

    Fix groovy import

--- src/test/java/groovy/ParameterUtilSpock.groovy
+++ src/test/java/groovy/ParameterUtilSpock.groovy
@@ -12,34 +12,34 @@ class ParameterUtilSpock extends Specification {
 
     def 'Check STAR is used by correct method injection when check all param is disabled'() {
         
-        when: parameterUtil.initQueryString('http://127.0.0.1?a=a*')
-        and: parameterUtil.initHeader('')
-        and: parameterUtil.initRequest('')
+        when: parameterUtil.initializeQueryString('http://127.0.0.1?a=a*')
+        and: parameterUtil.initializeHeader('')
+        and: parameterUtil.initializeRequest('')
         and:
             injectionModel.mediatorUtils.connectionUtil.methodInjection = injectionModel.mediatorMethodInjection.header
             parameterUtil.checkParametersFormat()
         then: thrown InjectionFailureException
         
-        when: parameterUtil.initQueryString('http://127.0.0.1')
-        and: parameterUtil.initHeader('a: a*')
-        and: parameterUtil.initRequest('')
+        when: parameterUtil.initializeQueryString('http://127.0.0.1')
+        and: parameterUtil.initializeHeader('a: a*')
+        and: parameterUtil.initializeRequest('')
         and:
             injectionModel.mediatorUtils.connectionUtil.methodInjection = injectionModel.mediatorMethodInjection.request
             parameterUtil.checkParametersFormat()
         then: thrown InjectionFailureException
         
-        when: parameterUtil.initQueryString('http://127.0.0.1')
-        and: parameterUtil.initHeader('')
-        and: parameterUtil.initRequest('a=a*')
+        when: parameterUtil.initializeQueryString('http://127.0.0.1')
+        and: parameterUtil.initializeHeader('')
+        and: parameterUtil.initializeRequest('a=a*')
         and:
             injectionModel.mediatorUtils.connectionUtil.methodInjection = injectionModel.mediatorMethodInjection.query
             parameterUtil.checkParametersFormat()
         then: thrown InjectionFailureException
     }
 
-    def 'Check that empty querystring, request and header is not allowed'() {
+    def 'Check that empty query string, request and header is not allowed'() {
         
-        when: parameterUtil.initQueryString('http://127.0.0.1')
+        when: parameterUtil.initializeQueryString('http://127.0.0.1')
         
         and:
             injectionModel.mediatorUtils.connectionUtil.methodInjection = injectionModel.mediatorMethodInjection.query
@@ -59,9 +59,9 @@ class ParameterUtilSpock extends Specification {
 
     def 'Check STAR is used one time only'() {
         
-        when: parameterUtil.initQueryString('http://127.0.0.1?a=a*&b=b')
-        and: parameterUtil.initRequest('a=a*&b=b')
-        and: parameterUtil.initHeader('''
+        when: parameterUtil.initializeQueryString('http://127.0.0.1?a=a*&b=b')
+        and: parameterUtil.initializeRequest('a=a*&b=b')
+        and: parameterUtil.initializeHeader('''
             a: a*
             b: b
         ''')
