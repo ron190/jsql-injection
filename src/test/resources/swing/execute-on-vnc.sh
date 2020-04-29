b9f25f90840fef18c549e99b6367f7549bfdabb4
ron190 <ron190@ymail.com>
2020-04-29 23:57:28 +0200

    Create .vnc/passwd

---
+++ src/test/resources/swing/execute-on-vnc.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+NEW_DISPLAY=42
+DONE="no"
+
+while [ "$DONE" == "no" ]
+do
+  out=$(xdpyinfo -display :${NEW_DISPLAY} 2>&1)
+  if [[ "$out" == name* ]] || [[ "$out" == Invalid* ]]
+  then
+    # command succeeded; or failed with access error;  display exists
+    (( NEW_DISPLAY+=1 ))
+  else
+    # display doesn't exist
+    DONE="yes"
+  fi
+done
+
+echo "Using first available display :${NEW_DISPLAY}"
+
+mkdir $HOME/.vnc/
+vncpasswd -f > $HOME/.vnc/passwd <<EOF
+123456
+123456
+EOF
+
+OLD_DISPLAY=${DISPLAY}
+vncserver ":${NEW_DISPLAY}" -localhost -geometry 1600x1200 -depth 16
+export DISPLAY=:${NEW_DISPLAY}
+
+"$@"
+
+export DISPLAY=${OLD_DISPLAY}
+vncserver -kill ":${NEW_DISPLAY}"
