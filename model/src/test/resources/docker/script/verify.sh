01d960a72be136dd8c736ecd106115005a576db9
ron190 <ron190@ymail.com>
2020-05-02 18:59:24 +0200

    Add circleci config and fix docker exec

--- model/src/test/resources/docker/script/verify.sh
+++ model/src/test/resources/docker/script/verify.sh
@@ -1,27 +1,30 @@
 # Buff MySQL
-docker exec -i jsql-mysql /bin/bash << EOF
-   mysql -uroot -pmy-secret-pw -e '
-   SET GLOBAL max_connections = 100000;
-   SET GLOBAL thread_cache_size = 16384;
-   SET GLOBAL table_open_cache = 524288;
+buffMysql="
+   mysql -uroot -pmy-secret-pw -e ' \
+   SET GLOBAL max_connections = 100000; \
+   SET GLOBAL thread_cache_size = 16384; \
+   SET GLOBAL table_open_cache = 524288; \
    ';
-EOF
+"
+docker exec -i jsql-mysql /bin/bash -c ${buffMysql}
 
 # Check MySQL status
-docker exec -i jsql-mysql /bin/bash << EOF
-   mysql -uroot -pmy-secret-pw -e '
-   SHOW GLOBAL variables WHERE Variable_name RLIKE \"thread_cache_size|^max_connections|table_open_cache\";
-   SHOW GLOBAL status WHERE Variable_name RLIKE \"Threads_cached|Max_used_connections|Threads_created|Connections|Opened_tables|Threads_connected|Threads_running|^Queries\"
+checkMysql="
+   mysql -uroot -pmy-secret-pw -e ' \
+   SHOW GLOBAL variables WHERE Variable_name RLIKE \"thread_cache_size|^max_connections|table_open_cache\"; \
+   SHOW GLOBAL status WHERE Variable_name RLIKE \"Threads_cached|Max_used_connections|Threads_created|Connections|Opened_tables|Threads_connected|Threads_running|^Queries\" \
    ';
-EOF
+"
+docker exec -i jsql-mysql /bin/bash -c ${checkMysql}
 
 # Check Postgres status
-docker exec -i jsql-postgres /bin/bash << EOF
-   export PGPASSWORD=my-secret-pw;
-   psql -U postgres -h 127.0.0.1 -d \"\" -e -a -c '
-   show max_connections;
-   ';
-EOF
+checkPostgre="
+    export PGPASSWORD=my-secret-pw;
+    psql -U postgres -h 127.0.0.1 -d \"\" -e -a -c ' \
+        show max_connections; \
+    '
+"
+docker exec -i jsql-postgres /bin/bash -c ${checkPostgre}
    
 docker exec -i jsql-sqlserver /opt/mssql-tools/bin/sqlcmd \
 -S "tcp:127.0.0.1,1434" \
