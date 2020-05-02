3c934630730d24d4a046d6c335013c7f0eea6913
ron190 <ron190@ymail.com>
2020-05-02 19:20:36 +0200

    Fix docker exec

--- model/src/test/resources/docker/script/verify.sh
+++ model/src/test/resources/docker/script/verify.sh
@@ -1,32 +1,29 @@
 # Buff MySQL
-buffMysql="
-   mysql -uroot -pmy-secret-pw -e ' \
-   SET GLOBAL max_connections = 100000; \
-   SET GLOBAL thread_cache_size = 16384; \
-   SET GLOBAL table_open_cache = 524288; \
-   ';
-"
-docker exec -i jsql-mysql /bin/bash -c ${buffMysql}
+cat <<EOF | sudo docker exec -i jsql-mysql /bin/bash
+    mysql -uroot -pmy-secret-pw -e ' 
+        SET GLOBAL max_connections = 100000; 
+        SET GLOBAL thread_cache_size = 16384; 
+        SET GLOBAL table_open_cache = 524288; 
+    ';
+EOF
 
 # Check MySQL status
-checkMysql="
-   mysql -uroot -pmy-secret-pw -e ' \
-   SHOW GLOBAL variables WHERE Variable_name RLIKE \"thread_cache_size|^max_connections|table_open_cache\"; \
-   SHOW GLOBAL status WHERE Variable_name RLIKE \"Threads_cached|Max_used_connections|Threads_created|Connections|Opened_tables|Threads_connected|Threads_running|^Queries\" \
+cat <<EOF | sudo docker exec -i jsql-mysql /bin/bash
+   mysql -uroot -pmy-secret-pw -e ' 
+       SHOW GLOBAL variables WHERE Variable_name RLIKE "thread_cache_size|^max_connections|table_open_cache"; 
+       SHOW GLOBAL status WHERE Variable_name RLIKE "Threads_cached|Max_used_connections|Threads_created|Connections|Opened_tables|Threads_connected|Threads_running|^Queries" 
    ';
-"
-docker exec -i jsql-mysql /bin/bash -c ${checkMysql}
+EOF
 
 # Check Postgres status
-checkPostgre="
+cat <<EOF | sudo docker exec -i jsql-postgres /bin/bash
     export PGPASSWORD=my-secret-pw;
-    psql -U postgres -h 127.0.0.1 -d \"\" -e -a -c ' \
-        show max_connections; \
+    psql -U postgres -h 127.0.0.1 -d "" -e -a -c ' 
+        show max_connections; 
     '
-"
-docker exec -i jsql-postgres /bin/bash -c ${checkPostgre}
+EOF
    
-docker exec -i jsql-sqlserver /opt/mssql-tools/bin/sqlcmd \
+sudo docker exec -i jsql-sqlserver /opt/mssql-tools/bin/sqlcmd \
 -S "tcp:127.0.0.1,1434" \
 -U SA \
 -P "yourStrong(!)Password" \
