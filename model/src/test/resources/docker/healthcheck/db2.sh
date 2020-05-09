4c9cee12c2c335792cf086039a085703a7cfb0a8
ron190 <ron190@ymail.com>
2020-05-10 01:25:57 +0200

    Connect to db2 IN SHARE MODE

--- model/src/test/resources/docker/healthcheck/db2.sh
+++ model/src/test/resources/docker/healthcheck/db2.sh
@@ -7,10 +7,15 @@ retry=0
 until cat <<EOF | docker exec -i --workdir /database/config/db2inst1/sqllib/bin --user db2inst1 jsql-db2 /bin/bash     
     
     pwd
-    export DB2INSTANCE=db2inst1                                                                                                                            
-    ./db2 list active databases                                 
-    ./db2 connect to testdb                              
-    ./db2 select 1 as jsqlColumn from sysibm.sysversions                    
+    export DB2INST1_PASSWORD=test
+    export DB2INSTANCE=db2inst1
+    export DB2LIB=/database/config/db2inst1/sqllib/lib
+    export DB2_HOME=/database/config/db2inst1/sqllib
+    export DBNAME=testdb
+    export DBPORT=50000
+    ./db2 list active databases
+    ./db2 connect to testdb IN SHARE MODE
+    ./db2 select 1 as jsqlColumn from sysibm.sysversions
 EOF
 do
   retry=$((retry+1))
