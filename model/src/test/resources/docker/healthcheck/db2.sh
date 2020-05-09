d9c3a90f142c1f6f2e01e396b756a61d96216559
ron190 <ron190@ymail.com>
2020-05-10 00:35:35 +0200

    Increase db2 timeout

--- model/src/test/resources/docker/healthcheck/db2.sh
+++ model/src/test/resources/docker/healthcheck/db2.sh
@@ -7,13 +7,14 @@ retry=0
 until cat <<EOF | docker exec -i --user db2inst1 jsql-db2 /bin/bash     
     
     pwd
-    export DB2INSTANCE=db2inst1                                                                                                                            
-    ./database/config/db2inst1/sqllib/bin/db2 connect to testdb                                  
+    export DB2INSTANCE=DB2INST1                                                                                                                            
+    ./database/config/db2inst1/sqllib/bin/db2 list active databases                                 
+    ./database/config/db2inst1/sqllib/bin/db2 connect to testdb                              
     ./database/config/db2inst1/sqllib/bin/db2 select 1 as jsqlColumn from sysibm.sysversions                    
 EOF
 do
   retry=$((retry+1))
-  if [ $retry -gt 30 ] ; then
+  if [ $retry -gt 60 ] ; then
     exit 1
   fi
   
