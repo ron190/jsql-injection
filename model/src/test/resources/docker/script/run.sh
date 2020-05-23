e8dc28cc5d59d5ed048201204a2eeb6bfd8b99db
ron190 <ron190@ymail.com>
2020-05-24 00:54:08 +0200

    Pass ARG for MAVEN_NASHORN to Dockerfile

---
+++ model/src/test/resources/docker/script/run.sh
@@ -0,0 +1,13 @@
+docker run                                                    \
+    -v "$HOME/.m2":/root/.m2                                  \
+    -v "$HOME/.sonar/cache":/root/.sonar/cache                \
+    -e "SONARQUBE_SCANNER_PARAMS=${SONARQUBE_SCANNER_PARAMS}" \
+    -e "SONAR_TOKEN=${SONAR_TOKEN}"                           \
+    --network docker_jsql-network                             \
+    jsql:latest                                               \
+    ./model/src/test/resources/vnc/execute-on-vnc.sh          \
+    mvn clean verify sonar:sonar
+    
+export DOCKER_RUN=$?
+echo "docker run exit code: ${DOCKER_RUN}"
+if [ ${DOCKER_RUN} -ne 0 ]; then exit 1; fi
