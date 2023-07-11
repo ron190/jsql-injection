732da9e77cf6e2c1007af020aadd5027e3f25166
ron190 <ron190@ymail.com>
2023-07-11 20:44:50 +0200

    Scan into running container

--- model/src/test/resources/docker/scripts/exec-on-docker.sh
+++ model/src/test/resources/docker/scripts/exec-on-docker.sh
@@ -1,4 +1,4 @@
-docker exec -i jsql-mysql bash -c "$1"
+docker exec -i jsql-mysql ./model/src/test/resources/vnc/execute-on-vnc.sh bash -c "$1"
 
 DOCKER_RUN="$?"
 echo docker run exit code: $DOCKER_RUN
