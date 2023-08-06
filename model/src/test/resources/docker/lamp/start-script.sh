b5d5557034a8fcb286bdcfff05473e7fcd720892
ron190 <ron190@ymail.com>
2023-08-06 13:17:14 +0200

    Debug root grants

--- model/src/test/resources/docker/lamp/start-script.sh
+++ model/src/test/resources/docker/lamp/start-script.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-echo "
+echo '
 [mysqld]
-secure-file-priv = ''
-port = 3308
-" >> /etc/mysql/my.cnf
+secure-file-priv=""
+port=3308
+' >> /etc/mysql/my.cnf
 
 service mysql start
 
 mysql -uroot -ppassword -e "
   ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
   GRANT FILE ON *.* TO 'root'@'%';
-"
-mysql -u root -ppassword -e "
-  SHOW VARIABLES LIKE 'secure_file_priv';
+  SHOW GRANTS;
+  SHOW VARIABLES;
   SHOW DATABASES;
 "
 
