4a9df9539fbc5da8fff95297f98e4173fc544e46
ron190 <ron190@ymail.com>
2023-10-08 21:54:11 +0200

    Restore user postgres in healthcheck

--- model/src/test/resources/docker/scripts/healthcheck/postgres.sh
+++ model/src/test/resources/docker/scripts/healthcheck/postgres.sh
@@ -4,7 +4,7 @@ set -e
 
 retry=0
 
-until docker exec -i jsql-postgresql psql -h "127.0.0.1" -U "postgresql" -c "select 'jsqlValue' as jsqlColumn"; do
+until docker exec -i jsql-postgresql psql -h "127.0.0.1" -U "postgres" -c "select 'jsqlValue' as jsqlColumn"; do
 
   retry=$((retry+1))
   if [ $retry -gt 30 ] ; then
