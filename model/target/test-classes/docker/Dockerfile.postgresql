9e717d68ae18fed9ef3edd912d5da2e97868886e
ron190 <ron190@ymail.com>
2023-10-21 22:39:26 +0200

    Cleaning

--- model/target/test-classes/docker/Dockerfile.postgresql
+++
@@ -1,3 +0,0 @@
-FROM postgres:9.6
-
-EXPOSE 5432
--- model/target/test-classes/docker/lamp/php/get-pg.php
+++
@@ -1,6 +0,0 @@
-<?php
-ini_set('display_errors', 1);
-$db = new PDO("pgsql:host=localhost;port=5432;dbname=test", "postgres", 'postgres');
-foreach($db->query("SELECT '1' FROM (select 1)x where '1'={$_GET['id']}") as $row) {
-    echo "<li>" . join(',', $row) . "</li>";
-}
