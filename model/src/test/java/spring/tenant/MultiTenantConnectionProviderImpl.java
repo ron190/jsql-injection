ad73524f2e653aae9eddfd31efa3ef1c06df50d5
ron190 <ron190@ymail.com>
2024-06-23 03:13:12 +0200

    Fixes hibernate new multitenant generic

--- model/src/test/java/spring/tenant/MultiTenantConnectionProviderImpl.java
+++ model/src/test/java/spring/tenant/MultiTenantConnectionProviderImpl.java
@@ -3,7 +3,7 @@ package spring.tenant;
 import org.hibernate.engine.jdbc.connections.spi.AbstractMultiTenantConnectionProvider;
 import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
 
-public class MultiTenantConnectionProviderImpl extends AbstractMultiTenantConnectionProvider {
+public class MultiTenantConnectionProviderImpl extends AbstractMultiTenantConnectionProvider<String> {
 
     private static final MasterService MASTER_SERVICE = new MasterService();
     
@@ -15,7 +15,7 @@ public class MultiTenantConnectionProviderImpl extends AbstractMultiTenantConnec
 
     @Override
     protected ConnectionProvider selectConnectionProvider(String tenant) {
-        
+
         return MASTER_SERVICE.getDataSourceHashMap().getOrDefault(
             tenant,
             // Provide default connection to prevent error logs when tenant is undefined
