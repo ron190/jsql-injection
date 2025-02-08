84cdc24014b4c620ee0f673c86a163cd295382f1
ron190 <ron190@ymail.com>
2025-02-08 15:21:17 +0100

    Bounce oracle listener

--- model/src/test/resources/docker/scripts/healthcheck/oracle.sh
+++ model/src/test/resources/docker/scripts/healthcheck/oracle.sh
@@ -17,14 +17,16 @@ do
   sleep 10
 done
 
+>&2 echo "Oracle is up - executing command"
+
 cat <<EOF | docker exec -i jsql-oracle /bin/bash
-echo '1/4 Status lsnrctl...' && lsnrctl status
-echo '2/4 Chmod log...' && chmod 777 /opt/oracle/product/18c/dbhomeXE/network/log/
-echo '3/4 Bouncing...' && cat <<EOF2 | lsnrctl
+echo '### 1/4 Status lsnrctl...' && lsnrctl status
+echo '### 2/4 Chmod log...' && chmod 777 /opt/oracle/product/18c/dbhomeXE/network/log/
+echo '### 3/4 Bouncing...' && cat <<EOF2 | lsnrctl
 stop
 start
 EOF2
-echo '4/4 Chmod oracle...' && chmod 6751 \$ORACLE_HOME/bin/oracle
+echo '### 4/4 Chmod oracle...' && chmod 6751 \$ORACLE_HOME/bin/oracle
 EOF
 
 retry=0
@@ -35,7 +37,7 @@ select 'jsqlValue' as jsqlColumn from dual;
 EOF
 do
   retry=$((retry+1))
-  if [ $retry -gt $((10)) ] ; then
+  if [ $retry -gt $((60*10)) ] ; then
     exit 1
   fi
 
